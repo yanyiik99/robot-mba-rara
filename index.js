f03f14713df5c472db1591b979912c5588ccfabd
yanyiik99 <aripramana574@gmail.com>
2022-06-17 09:32:38 -0700

    Menambahkan API Translate ke bahasa Indonesia

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const clickRamalan = document.getElementById("clickRamalan");
 
 clickRamalan.addEventListener("click", ()=>{
 
+
+
   async function ramalan(){
-    
+
+    // AOS.init({
+    //   once: true,
+    // });
+
     const response = await 
     fetch("https://api.adviceslip.com/advice", {
     method: "GET"
@@ -16,13 +22,7 @@ clickRamalan.addEventListener("click", ()=>{
     console.log(json);
     const results =  json.slip.advice; // Pesan Ramalananya
     
-      //Menampilkan hasil ramalan
-    const ramalan = document.getElementById("ramalan");
-    ramalan.innerHTML = `"${results}"`;
-    ramalan.style.fontFamily = "'Permanent Marker', cursive";
-    ramalan.style.fontSize = "24px";
-    ramalan.style.display = "block";
-
+    
       //Menampilkan hasil Nama
     const nama = document.getElementById("nama").value;
     const tampilNama = document.getElementById("tampilNama");
@@ -33,19 +33,60 @@ clickRamalan.addEventListener("click", ()=>{
     
     //Menampilkan Nama
     tampilNama.innerHTML = `Hai ${nama}`;
-
     tampilNama.style.textAlign = "center";
     tampilNama.style.paddingBottom = "40px";
     tampilNama.style.fontFamily = "'Permanent Marker', cursive";
 
-    AOS.init({
-      once: true,
-    });
+
+
+    // Translate API
+    // const bahan = '[{"Text":"' + results +'"}]';
+    const soureText = `[{"Text": "${results}"}]`;
+    
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '43622e8775msh30b86e8dad97a81p14b9e0jsnf658c5965102',
+        'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+      },
+      body: soureText
+    };
+
+    const translate = await fetch('https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=id&api-version=3.0&profanityAction=NoAction&textType=plain', options)
+      .then(response => response.json())
+      .then(response => {
+        return `"${response[0].translations[0].text}"`;})
+      .catch(err => console.error(err));
+      
+    
+    
+    const ramalan = document.getElementById("ramalan");
+    ramalan.innerHTML = translate;
+    ramalan.style.fontFamily = "'Permanent Marker', cursive";
+    ramalan.style.fontSize = "24px";
+    ramalan.style.display = "block";
+
+    AOS.init({once: true});
+
+
+
+    // fetch('https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=id&api-version=3.0&profanityAction=NoAction&textType=plain', options)
+    //   .then(response => response.json())
+    //   // .then(callAOS())
+    //   .then(response => {
+    //       //Menampilkan hasil ramalan
+    //       const ramalan = document.getElementById("ramalan");
+    //       ramalan.innerHTML = `" ${response[0].translations[0].text} "`;
+    //       ramalan.style.fontFamily = "'Permanent Marker', cursive";
+    //       ramalan.style.fontSize = "24px";
+    //       ramalan.style.display = "block";})
+    //   .catch(err => console.error(err));
+
+
 
   }
-
   ramalan();
-
 
 
 });
