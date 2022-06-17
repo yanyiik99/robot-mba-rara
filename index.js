e69372c460908ac5c8894f4615cc55d71b621c09
yanyiik99 <aripramana574@gmail.com>
2022-06-17 01:35:37 -0700

    inisialisasi commit pertama

---
+++ index.js
@@ -0,0 +1,47 @@
+ //Function Fetch API ke advice 
+
+const clickRamalan = document.getElementById("clickRamalan");
+
+
+clickRamalan.addEventListener("click", ()=>{
+
+  async function ramalan(){
+        
+    const response = await 
+    fetch("https://api.adviceslip.com/advice", {
+    method: "GET"
+    });
+
+    const json = await response.json();
+    console.log(json);
+    const results =  json.slip.advice; // Pesan Ramalananya
+    
+      //Menampilkan hasil ramalan
+    const ramalan = document.getElementById("ramalan");
+    ramalan.innerHTML = `"${results}"`;
+    ramalan.style.fontFamily = "'Permanent Marker', cursive";
+    ramalan.style.fontSize = "24px";
+
+      //Menampilkan hasil Nama
+    const nama = document.getElementById("nama").value;
+    const tampilNama = document.getElementById("tampilNama");
+
+    //Menghilangkan Form setelah click tombol ramal 
+    const inputNama = document.getElementById("inputNama").style.display = "none";
+
+    
+    //Menampilkan Nama
+    tampilNama.innerHTML = `Hai ${nama}`;
+
+    tampilNama.style.textAlign = "center";
+    tampilNama.style.paddingBottom = "40px";
+    tampilNama.style.fontFamily = "'Permanent Marker', cursive";
+
+  }
+
+  ramalan();
+
+  
+
+
+});
